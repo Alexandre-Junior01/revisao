9a8629aeac87b237029419884db67bc478ff37ec
Alexandre-Junior01 <alexandrebia00@gmail.com>
2025-04-22 11:10:54 -0300

    alteração

--- rev07.py
+++ rev07.py
@@ -1,5 +1,13 @@
 """ usando FOR, mostre numeros impar """
 
-for i in range(1,500):
+"""for i in range(1,500):
    if i % 2 !=0:
-       print(i)
+       print(i)"""
+
+
+""" usando o while"""
+i =1
+while i<500:
+    if i % 2 !=0:
+        print(i)
+    i+=1
