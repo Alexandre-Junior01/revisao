cd83d8fd7a1a77a9d2e0057a4082ac4b0fef62dc
Alexandre-Junior01 <alexandrebia00@gmail.com>
2025-04-22 11:26:24 -0300

    codigo novo

--- rev07.py
+++ rev07.py
@@ -1,13 +1,29 @@
 """ usando FOR, mostre numeros impar """
 
-"""for i in range(1,500):
+"""for i in range(1,500+1):
    if i % 2 !=0:
        print(i)"""
 
-
 """ usando o while"""
-i =1
-while i<500:
-    if i % 2 !=0:
+"""
+ i =1
+while i<=500:
+    if i % 2 ==1:
         print(i)
     i+=1
+    
+ """
+
+" usando for, mostre os numeros pares de 1 a 100, ordem descrescente"""
+
+"""
+for i in range(100,1,-1):
+    print(i) 
+    
+    """
+
+x = 100
+while x >= 1:
+    if x % 2 == 0:
+        print(x)
+    x -= 1
